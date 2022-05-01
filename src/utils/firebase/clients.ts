59c73811c51216df8dcf25d36ed79781dd8adba7
Mario Muniz <mario@mariomuniz.com>
2022-04-30 18:06:47 -0700

    Use base64 encoded credentials

--- src/utils/firebase/clients.ts
+++ src/utils/firebase/clients.ts
@@ -1,8 +1,18 @@
-import { applicationDefault, initializeApp } from 'firebase-admin/app'
+import firebaseAdmin from 'firebase-admin'
+import { initializeApp } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 
+const { credential } = firebaseAdmin
+
 initializeApp({
-  credential: applicationDefault(),
+  credential: credential.cert(
+    JSON.parse(
+      Buffer.from(
+        process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64,
+        'base64',
+      ).toString('ascii'),
+    ),
+  ),
 })
 
 export const db = getFirestore()
